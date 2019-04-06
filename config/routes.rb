eebd723ebcab2c28f9f918545ca977db56de2693
Niko Dyer <nikodyer44@gmail.com>
2019-04-06 11:51:48 -0600

    fixed controller issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+
+    resources :users do
+      resources :videos
+    end
+
+    resources :videos do
+      resources :comments
+    end
+
+  end
 end
