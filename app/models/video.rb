fb870c410547d1c120f9fc07e072607c71b877e9
3mpirical <3mpirical10510@gmail.com>
2019-04-06 10:26:00 -0600

    created seeds file and checked user auth

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,4 @@
 class Video < ApplicationRecord
     has_many :comments
+    belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all()
+
+
+User.create(
+    email: "test@test.com",
+    password: "password",
+)
+
+for i in (1..10)
+    user = User.create(
+        email: Faker::Internet.email(),
+        password: "password",
+    )
+
+    puts user.id
+
+    Video.create(
+        title: "title",
+        name: "name",
+        duration: 20,
+        genre: "genre",
+        description: "description",
+        trailer: "trailer",
+        user_id: user.id,
+    )
+
+    puts User.find(user.id).videos[0].title
+
+    # t.string "title"
+    # t.string "name"
+    # t.integer "duration"
+    # t.string "genre"
+    # t.string "description"
+    # t.string "trailer"
+end
+
+puts "_____Database_Seeded_____"
+
